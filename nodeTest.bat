6b7edcc51cddc6c3f8e6867ed9d945066ba45ce6
azrafe7 <azrafe7@gmail.com>
2017-04-09 19:49:41 +0200

    nodeTest.bat: exit on compilation error

--- nodeTest.bat
+++ nodeTest.bat
@@ -1,6 +1,10 @@
 @echo off
 echo Compiling...
 call haxe test.hxml -js bin\testall.js -lib hxnodejs
+if '%ERRORLEVEL%'=='0' goto next
+goto :eof
+
+:next
 echo DONE
 echo Press a key to debug with node devtool...
 pause > NUL
